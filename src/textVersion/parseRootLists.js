080eefb6af4256a66b5686bf35d33f8611f2156d
kaylea box <kayleabox09@gmail.com>
2021-11-28 00:50:52 -0600

    sloppy work on four letter roots and longer roots

--- src/textVersion/parseRootLists.js
+++ src/textVersion/parseRootLists.js
@@ -11,6 +11,9 @@ const hansWehrText = fs.readFileSync('src/textVersion/hansWehr.txt', 'utf8' , (e
 const isArabic = /[\u0600-\u06FF]/;
 const isNumberic = /^\d+$/;
 
+// hamzas, tamarbuta, vowels
+// /[\u0620-\u0627|\u0629|\u0648-\u064A]/
+
 const linesArray = hansWehrText.split('\n');
 
 const { twoLetterRootsArray,  threeLetterRootsArray, fourLetterRootsArray, longRootsArray } = parseRootLists(linesArray);
@@ -31,8 +34,8 @@ fs.writeFileSync('src/textVersion/parsedFiles/sortedThreeLetterRootSet.txt', [..
 function getThreeLetterRootsWithLongVowels(rootArray){
     const rootsWithLongVowels = [];
     rootArray.forEach(root => {
-        const vowels = ['ا', 'و', 'ي'];
-        if(vowels.includes(root[1] || root[2])){
+      const vowels = ['ا', 'و', 'ي', 'ى'];
+      if(vowels.includes(root[1] || root[2])){
             rootsWithLongVowels.push(root);
         }
     })
@@ -51,18 +54,19 @@ function parseRootLists(lines) {
   lines.forEach(line => {
     const lineWordsArray = line.split(' ');
     const firstWord = lineWordsArray[0];
-    const vowels = ['ا', 'و', 'ي', 'ى'];
-    const hamzas = ['أ', 'ئ', 'ء', 'ؤ'];
+    const vowels = ['ا', 'و', 'ي', 'ى', 'آ', 'ة'];
+    const hamzas = ['أ', 'إ', 'ئ', 'ء', 'ؤ'];
   
     if(isArabic.test(firstWord)) {
-        let firstWordArabic = firstWord.replace(/[^\u0600-\u06FF]|[\u0651]/g, '');
+        let firstWordArabic = firstWord.replace(/[^\u0600-\u06FF]|[\u0651]|[\u060C]/g, '');
     
         if(firstWordArabic.length === 1 && line.length === 1) {
           charArray.push(firstWordArabic);
         } else if (firstWordArabic[0] === charArray[charArray.length - 1]) {
-          // need to handle special characters that are the same letter but a different character
+          // @TODO need to handle special characters that are the same letter but a different character
 
           // build root arrays
+          
           // two letter root
           if (firstWordArabic.length === 2) {
             // if (twoLetterRootsArray.includes(firstWordArabic)) {}
@@ -78,6 +82,7 @@ function parseRootLists(lines) {
             // if (threeLetterRootsArray.includes(firstWordArabic)) {}
             const previousTwoLetterRoot = twoLetterRootsArray[twoLetterRootsArray.length - 1];
             const previousThreeLetterRoot = threeLetterRootsArray[threeLetterRootsArray.length - 1];
+
             if (threeLetterRootsArray.length === 0 || previousThreeLetterRoot !== firstWordArabic){
               if (firstWordArabic[1] === firstWordArabic[2] && firstWordArabic.includes(previousTwoLetterRoot)){
                 // console.log(`previous: ${previousTwoLetterRoot}`);
@@ -127,8 +132,6 @@ function parseRootLists(lines) {
               ) { 
                 // console.log(`previous: ${previousThreeLetterRoot}`);
                 // console.log(`current: ${firstWordArabic}`);
-              } else if (firstWordArabic[2] === 'ي' && firstWordArabic.includes()) {
-
               } else if (
                 vowels.includes(firstWordArabic[1]) 
                 && previousTwoLetterRoot[0] === firstWordArabic[0]
@@ -149,50 +152,174 @@ function parseRootLists(lines) {
           // check that it does not contain the last three letter root with 
           // ا ء و ي ئ ؤ ى ة
           if (
-            firstWordArabic.length === 4 
-            && /[^\u0629|\u0621]/.test(firstWordArabic[3])
+            firstWordArabic.length === 4
+            // does not end in 'ة'
+            && firstWordArabic[3] !== 'ة'
           ) {
             // if (fourLetterRootsArray.includes(firstWordArabic)) {}
 
+            const previousTwoLetterRoot = twoLetterRootsArray.length > 0 ? twoLetterRootsArray[twoLetterRootsArray.length - 1] : '';
             // remove hamzas and vowels
             const previousThreeLetterRoot = threeLetterRootsArray.length > 0 ? threeLetterRootsArray[threeLetterRootsArray.length - 1] : '';
-            const includesPreviousThreeLetterRoot = previousThreeLetterRoot.split('').every(char => firstWordArabic.includes(char));
+            const includesPreviousThreeLetterRoot = previousThreeLetterRoot.split('').every(char => {
+              return firstWordArabic.includes(char) 
+              || (vowels.includes(char) && firstWordArabic.split('').some(c => vowels.includes(c))) 
+              || (hamzas.includes(char) && firstWordArabic.split('').some(c => hamzas.includes(c)));
+            });
+
             let extraChar = '';
-            firstWordArabic.split('').forEach((char, i) => {     
-              if (i < 3 && char !== previousThreeLetterRoot.split('')[i]) {
+            if(includesPreviousThreeLetterRoot){
+              firstWordArabic.split('').forEach((char) => {     
                 if (!previousThreeLetterRoot.includes(char)) {          
                   extraChar = char;
                 }
-              } else if (!previousThreeLetterRoot.includes(char)) {          
-                extraChar = char;
-              }
-            });
+              });
+            }
             
-            // console.log(`current root: ${firstWordArabic}`);
-            // console.log(`previous root: ${previousThreeLetterRoot}`);
-            // console.log(`extraChar: ${extraChar}`);
-            // console.log(`includes previous root: ${includesPreviousRoot}`);
-            // console.log(`regex result: ${/[\u0620-\u0627|\u0629|\u0648-\u064A]/.test(extraChar)}`);
-
-            if (includesPreviousThreeLetterRoot && /[\u0620-\u0627|\u0629|\u0648-\u064A]/.test(extraChar)) {
-              const vowelessWord = firstWordArabic.replace(extraChar, '');
-              if (previousThreeLetterRoot === vowelessWord) {
-                //console.log(vowelessWord);
-              } 
-            } else {
-              const previousFourLetterRoot = fourLetterRootsArray[fourLetterRootsArray.length - 1];
-              if(fourLetterRootsArray.length === 0 || previousFourLetterRoot !== firstWordArabic){
-                fourLetterRootsArray.push(firstWordArabic);
+            const previousFourLetterRoot = fourLetterRootsArray[fourLetterRootsArray.length - 1];
+            if(fourLetterRootsArray.length === 0 || previousFourLetterRoot !== firstWordArabic){
+              if ([...vowels, ...hamzas].includes(extraChar)) {
+                // console.log(`previous: ${previousThreeLetterRoot}`);
+                // console.log(`current: ${firstWordArabic}`);
+              } else if(
+                (firstWordArabic.slice(1) === previousThreeLetterRoot 
+                || firstWordArabic.slice(1) === previousTwoLetterRoot + previousTwoLetterRoot[1])
+                && firstWordArabic.startsWith('م')) {
+                // console.log(`previous: ${previousThreeLetterRoot}`);
+                // console.log(`current: ${firstWordArabic}`);
+              }  else if (
+                vowels.includes(firstWordArabic[1]) 
+                && vowels.includes(firstWordArabic[3]) 
+                && previousTwoLetterRoot[0] === firstWordArabic[0]
+                && previousTwoLetterRoot[1] === firstWordArabic[2]
+                && previousTwoLetterRoot > previousThreeLetterRoot
+              ) {
+                //  console.log(`previous three: ${previousThreeLetterRoot}`);
+                //  console.log(`previous two: ${previousTwoLetterRoot}`);
+                //  console.log(`current: ${firstWordArabic}`);
+              } else if (
+                previousTwoLetterRoot 
+                && vowels.includes(firstWordArabic[2]) 
+                && (previousTwoLetterRoot[0] === firstWordArabic[0]
+                || vowels.includes(firstWordArabic[0]) && vowels.includes(previousTwoLetterRoot[0])) 
+                && (previousTwoLetterRoot[1] === firstWordArabic[1]
+                || vowels.includes(firstWordArabic[1]) && vowels.includes(previousTwoLetterRoot[1]))
+                && previousTwoLetterRoot[1] === firstWordArabic[3]
+              ) { 
+                // console.log(`previous: ${previousTwoLetterRoot}`);
+                // console.log(`current: ${firstWordArabic}`);
+              } else if (
+                previousThreeLetterRoot 
+                && vowels.includes(firstWordArabic[2]) 
+                && (previousThreeLetterRoot[0] === firstWordArabic[0]
+                || vowels.includes(firstWordArabic[0]) && vowels.includes(previousThreeLetterRoot[0])) 
+                && (previousThreeLetterRoot[1] === firstWordArabic[1]
+                || vowels.includes(firstWordArabic[1]) && vowels.includes(previousThreeLetterRoot[1]))
+                && previousThreeLetterRoot[2] === firstWordArabic[3]
+              ) { 
+                // console.log(`previous: ${previousThreeLetterRoot}`);
+                // console.log(`current: ${firstWordArabic}`);
+              } else if (
+                previousThreeLetterRoot
+                && vowels.includes(firstWordArabic[1]) 
+                && (previousThreeLetterRoot[0] === firstWordArabic[0]
+                || vowels.includes(firstWordArabic[0]) && vowels.includes(previousThreeLetterRoot[0])) 
+                && previousThreeLetterRoot[1] === firstWordArabic[2]
+                && previousThreeLetterRoot[2] === firstWordArabic[3]
+              ) { 
+                // console.log(`previous: ${previousThreeLetterRoot}`);
+                // console.log(`current: ${firstWordArabic}`);
+              } else {
+                  fourLetterRootsArray.push(firstWordArabic);
               }
             }
           }
           
           // super long root!
           if (firstWordArabic.length > 4) {
-            const previousLongRoot = longRootsArray[longRootsArray.length - 1];
+            const previousLongRoot = longRootsArray.length > 0 ? longRootsArray[longRootsArray.length - 1] : '';
 
             if (longRootsArray.length === 0 || previousLongRoot !== firstWordArabic){
-              longRootsArray.push(firstWordArabic);
+              const previousTwoLetterRoot = twoLetterRootsArray.length > 0 ? twoLetterRootsArray[twoLetterRootsArray.length - 1] : '';
+              const previousThreeLetterRoot = threeLetterRootsArray.length > 0 ? threeLetterRootsArray[threeLetterRootsArray.length - 1] : '';
+              const includesPreviousThreeLetterRoot = previousThreeLetterRoot.split('').every(char => {
+                return firstWordArabic.includes(char) 
+                || (vowels.includes(char) && firstWordArabic.split('').some(c => vowels.includes(c))) 
+                || (hamzas.includes(char) && firstWordArabic.split('').some(c => hamzas.includes(c)));
+              });
+              const previousFourLetterRoot = fourLetterRootsArray.length > 0 ? fourLetterRootsArray[fourLetterRootsArray.length - 1] : '';
+              const includesPreviousFourLetterRoot = previousFourLetterRoot.split('').every(char => {
+                return firstWordArabic.includes(char) 
+                || (vowels.includes(char) && firstWordArabic.split('').some(c => vowels.includes(c))) 
+                || (hamzas.includes(char) && firstWordArabic.split('').some(c => hamzas.includes(c)));
+              });
+              const includesPreviousLongRoot = previousLongRoot.split('').every(char => {
+                return firstWordArabic.includes(char) 
+                || (vowels.includes(char) && firstWordArabic.split('').some(c => vowels.includes(c))) 
+                || (hamzas.includes(char) && firstWordArabic.split('').some(c => hamzas.includes(c)));
+              });
+
+              if (includesPreviousLongRoot
+                && firstWordArabic.split('').every(char => previousLongRoot.includes(char) || vowels.includes(char) || hamzas.includes(char)))
+              {
+                // console.log(previousFourLetterRoot);
+                // console.log(firstWordArabic.slice(0, 4));
+              }               
+              else if (includesPreviousFourLetterRoot
+                && firstWordArabic.split('').every(char => previousFourLetterRoot.includes(char) || vowels.includes(char) || hamzas.includes(char)))
+              {
+                // console.log(previousFourLetterRoot);
+                // console.log(firstWordArabic.slice(0, 4));
+              } 
+              else if (includesPreviousThreeLetterRoot
+                && firstWordArabic.split('').every(char => previousThreeLetterRoot.includes(char) || vowels.includes(char) || hamzas.includes(char)))
+              {
+                // console.log(previousThreeLetterRoot);
+                // console.log(firstWordArabic);
+              } 
+              else if (firstWordArabic.startsWith('است') 
+                && includesPreviousThreeLetterRoot
+                && firstWordArabic.slice(3).split('').every(char => previousThreeLetterRoot.includes(char) || vowels.includes(char) || hamzas.includes(char)))
+              {
+                // console.log(previousThreeLetterRoot);
+                // console.log(firstWordArabic);
+              }
+              else if (firstWordArabic.endsWith('ان') 
+              && includesPreviousFourLetterRoot
+              && firstWordArabic.length > 5
+              && firstWordArabic.slice(0, firstWordArabic.length - 2).split('').every(char => previousFourLetterRoot.includes(char) || vowels.includes(char) || hamzas.includes(char)))
+            {
+              // console.log(previousFourLetterRoot);
+              // console.log(firstWordArabic.slice(0, firstWordArabic.length - 2));
+              // console.log(firstWordArabic);
+            } 
+              else if (firstWordArabic.endsWith('ان') 
+              && includesPreviousThreeLetterRoot
+              && firstWordArabic.length > 4
+              && firstWordArabic.slice(0, firstWordArabic.length - 2).split('').every(char => previousThreeLetterRoot.includes(char) || vowels.includes(char) || hamzas.includes(char)))
+            {
+              // console.log(previousThreeLetterRoot);
+              // console.log(firstWordArabic.slice(0, firstWordArabic.length - 2));
+              // console.log(firstWordArabic);
+            } 
+            else if (firstWordArabic.endsWith('انى') 
+            && includesPreviousThreeLetterRoot
+            && firstWordArabic.length > 5
+            && firstWordArabic.slice(0, firstWordArabic.length - 3).split('').every(char => previousThreeLetterRoot.includes(char) || vowels.includes(char) || hamzas.includes(char)))
+          {
+            // console.log(previousThreeLetterRoot);
+            // console.log(firstWordArabic.slice(0, firstWordArabic.length - 3));
+            // console.log(firstWordArabic);
+          } 
+              else if (previousTwoLetterRoot.split('').every(char => firstWordArabic.includes(char))
+              && firstWordArabic.split('').every(char => previousTwoLetterRoot.includes(char) || vowels.includes(char) || hamzas.includes(char)))
+            {
+              // console.log(previousThreeLetterRoot);
+              // console.log(firstWordArabic);
+            } 
+              else {
+                longRootsArray.push(firstWordArabic);
+              }
             }
           }
 
